bfdd568517c584f617750f321459df3b7082d540
erica <erica@smaug.local>
2014-01-29 12:31:16 -0600

    Added nonbreaking spaces to footer-project.php

--- footer-project.php
+++ footer-project.php
@@ -2,7 +2,7 @@
  <div class="table padded-cell">
   <div class="table-cell">
    <div id="project-credit">
-    <p class="small">Screenshots courtesy client.<br />This site's design and all other content &copy;<?php date_default_timezone_set('America/Chicago'); echo date("Y"); ?> <a href="http://www.ericadreisbach.com">erica dreisbach</a>.
+    <p class="small">Screenshots courtesy client.<br />This site's design and all other content &copy;<?php date_default_timezone_set('America/Chicago'); echo date("Y"); ?>&nbsp;<a href="http://www.ericadreisbach.com">erica&nbsp;dreisbach</a>.
     </p>
    </div><!-- .text.workbox.foat-right -->
   </div><!-- .table-cell -->
