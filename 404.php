9967ff579e53ceaf91c604e246bc6eecf62a0996
erica <erica@smaug.local>
2014-01-30 17:34:50 -0600

    404 page files

---
+++ 404.php
@@ -0,0 +1,24 @@
+<?php $bodytype = 'page'; $subtitle = '404 error'; include('header.php'); include('page-nav.php'); ?>
+
+<section>
+ <div class="table padded-cell">
+  <div class="table-cell">
+   <h1 class="xtralarge">404</h1>
+   <h2>Page not found.</h2> 
+   <br />
+   <p>But don't worry. We'll get you where you need to go.</p>	
+  </div>
+ </div>
+</section>
+
+<section>
+ <div class="table padded-cell">
+  <div class="table-cell middle">
+   <h2><a href="http://www.ericadreisbach.com"><img src="img/logo-50-backwards.png" alt="main portfolio" />Main Portfolio</a></h2>
+   <h2><a href="email-invitation.php"><img src="img/email-portfolio.png" alt="email portfolio" />HTML Email Portfolio</a></h2>
+   <h2><a href="flash.php"><img src="img/flash.png" alt="Flash portfolio" />Selected Flash banners</a></h2>
+  </div><!-- .table-cell.middle -->
+ </div><!-- .table -->
+</section>  
+
+<?php include('footer.php'); ?>
